a4fbc149a5aa954227f4ce945f33457867789fe8
Matheus <M>
2017-10-30 22:20:34 -0200

    [2017-10-30 22:21] Criada classe criptografia

--- ChatJava/src/chatjava/Mensagem.java
+++ ChatJava/src/chatjava/Mensagem.java
@@ -7,11 +7,18 @@ public class Mensagem{
     final String corAzul="\u001B[34m";
     //
     public void enviarParaTodos(ArrayList<ClienteEmissor> arrayCliente,String mensagem){
-        mensagem=("["+new Tempo().getHoraMinutoAtual()+"] "+mensagem);
+        mensagem=("["+new Tempo().getHoraCompletaAtual()+"] "+mensagem);
         for(int i=0; i<arrayCliente.size(); i++){
             arrayCliente.get(i).enviarMensagem(mensagem);
             //new GerenciadorBD().inserirMensagem(mensagem); funcionando
         }
+        
+        String crip=new CriptografiaRSA().cifrar(mensagem);
+        String decrip=new CriptografiaRSA().decifrar(crip);
+        System.out.println("mensagem: "+mensagem);
+        System.out.println("Criptografada: "+crip);
+        System.out.println("Decrip: "+decrip);
+        
     }   
     public String mensagemErro(String erro){
         return(corVermelha+(erro)+corReset);
